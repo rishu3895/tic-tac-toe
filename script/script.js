4d8d44313308f56b8bd6d805216ab472742974bf
rishu3895 <rishabh.rsharma1999@gmail.com>
2020-11-18 15:55:02 +0530

    completed the two player game

--- script/script.js
+++ script/script.js
@@ -22,6 +22,8 @@ function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+
+    
 }
 //  factory function for creating elements
 const person = () => {
@@ -44,7 +46,59 @@ const person = () => {
         myButton.classList=buttonClass;
         return {myButton};
     }
-    return {myHeading, myInput, myButton};
+    const myDiv = (divClass, divHtml) => {
+        const myDiv=document.createElement('div');
+        myDiv.classList=divClass;
+        myDiv.innerHtml=divHtml;
+        return {myDiv};
+    }
+    
+
+    return {myHeading, myInput, myButton, myDiv};
+}
+const myDivs = () => {
+    const mainDiv=person().myDiv('main-grid-div','');
+    const div1=person().myDiv('top-left grid-item','');
+    const div2=person().myDiv('top-middle grid-item','');
+    const div3=person().myDiv('top-right grid-item','');
+    const div4=person().myDiv('middle-left grid-item','');
+    const div5=person().myDiv('middle-middle grid-item','');
+    const div6=person().myDiv('middle-right grid-item','');
+    const div7=person().myDiv('lower-left grid-item','');
+    const div8=person().myDiv('lower-middle grid-item','');
+    const div9=person().myDiv('lower-right grid-item','');
+    for(let i=1;i<10;i++){
+        const temp=`div${i}.myDiv`;
+        mainDiv.myDiv.appendChild(eval(temp));
+    }
+    return {mainDiv};
+}
+// single player page
+function singlePlayerGame(player1, player2){
+    const heading1=person().myHeading('heading-player-one-name player-name',player1);
+    const heading2=person().myHeading('heading-player-two-name player-name',player2);
+    const div=document.createElement('div');
+    div.classList="game-div";
+    div.appendChild(heading1.heading1); 
+    div.appendChild(myDivs().mainDiv.myDiv);
+    div.appendChild(heading2.heading1);
+    game.appendChild(div);
+
+    const lowerDiv=document.createElement('div');
+    const resetBtn=person().myButton('reset-btn lower-btn','RESET');
+    const mainMenuBtn=person().myButton('main-menu-btn lower-btn','MAIN MENU');
+    lowerDiv.appendChild(resetBtn.myButton);
+    lowerDiv.appendChild(mainMenuBtn.myButton);
+    game.appendChild(lowerDiv);
+    document.querySelector('.main-menu-btn').addEventListener('click',()=>{
+        removeAllChildNodes(game);
+        startingPage();
+        eventListener();
+    });
+    document.querySelector('.reset-btn').addEventListener('click',()=>{
+        resetGrid();
+    });
+    playGame();
 }
 function singlePlayerPage() {
     const heading1 = person().myHeading('heading-player1', 'Player1');
@@ -52,24 +106,50 @@ function singlePlayerPage() {
     const playGame = person().myButton('play-game','Play game');
     game.appendChild(heading1.heading1);
     game.appendChild(playerName.input1);
+    playGame.myButton.classList.add("single-player-game");
     game.appendChild(playGame.myButton);
+    document.querySelector('.single-player-game').addEventListener('click', () => {
+        removeAllChildNodes(game);
+        var playerOneName=playerName.input1.value;
+        if(playerOneName===""){
+            playerOneName="Player 1";
+        }
+        singlePlayerGame(playerOneName,'Computer');
+    });
+    
 }
-
+// multi player page
 function twoPlayerPage() {
     const heading1=person().myHeading('heading-player1','Player 1');
     const playerName=person().myInput('text','Enter your name','enter-name');
     const heading2 = person().myHeading('heading-player1','Player 2');
     const playerName2 =person().myInput('text','Enter your name','enter-name');
     const playGame = person().myButton('play-game','Play game');
+    playGame.myButton.classList.add("multi-player-game");
     game.appendChild(heading1.heading1);
     game.appendChild(playerName.input1);
     game.appendChild(heading2.heading1);
     game.appendChild(playerName2.input1);
     game.appendChild(playGame.myButton);
+    document.querySelector('.multi-player-game').addEventListener('click', () => {
+        removeAllChildNodes(game);
+        var playerOneName=playerName.input1.value;
+        var playerTwoName=playerName2.input1.value;
+        if(playerOneName==''){
+            playerOneName='player 1';
+        }
+        if(playerTwoName===''){
+            playerTwoName='player 2';
+        }
+        singlePlayerGame(playerOneName,playerTwoName);
+    });
+    
+
+
 }
 startingPage();
 
-// event listeners
+
 const eventListener = () => {
     document.querySelector('.single-player-button').addEventListener('click', () => {
         removeAllChildNodes(game);
@@ -78,6 +158,108 @@ const eventListener = () => {
     document.querySelector('.two-player-button').addEventListener('click', () => {
         removeAllChildNodes(game);
         twoPlayerPage();
-    })
+    });
+    
+  
 }
 eventListener();
+
+// the game begins
+let innerValue='X';
+const changeValue = () => {
+    if(innerValue=='X'){
+        innerValue='O';
+    }
+    else{
+        innerValue='X';
+    }
+}
+const handleCellClick = (e) => {
+    const location=e.target.classList[0];
+    // let tempValue=innerValue;
+    // document.querySelector(`.${location}`).innerHTML=tempValue;
+    arrayValueInsert(location);
+}
+const playGame = () => {
+    const gridCells=document.querySelectorAll('.grid-item');
+    for(gridCell of gridCells){
+        gridCell.addEventListener('click', handleCellClick);
+    }
+}
+const findArrayNumber = (location) => {
+    if(location == 'top-left')return 0;
+    if(location == 'top-middle')return 1;
+    if(location == 'top-right')return 2;
+    if(location == 'middle-left')return 3;
+    if(location == 'middle-middle')return 4;
+    if(location == 'middle-right')return 5;
+    if(location == 'lower-left')return 6;
+    if(location == 'lower-middle')return 7;
+    if(location == 'lower-right')return 8;
+}
+let arr=[0,0,0,
+         0,0,0,
+         0,0,0];
+const arrayValueInsert = (location) => {
+    const arrayValue=findArrayNumber(location);
+    if(arr[arrayValue]==0){
+        arr[arrayValue]=innerValue;
+        changeValue();
+    }
+    render();
+    checkGame();
+}
+const render = () => {
+    const cellSelector=document.querySelectorAll('.grid-item');
+    let i=0;
+    for(sel of cellSelector){
+        if(arr[i]!=0){
+            sel.innerHTML=arr[i];
+        }
+        else{
+            sel.innerHTML='';
+        }
+        i++;
+    }
+}
+function check(a,b,c){
+    if(arr[a]==arr[b] && arr[b]==arr[c] && arr[a]!=0){
+        const winner=person().myHeading('winner-class','WINNER');
+        if(arr[a]=='X'){
+            const winnerA=document.querySelector('.heading-player-one-name');
+            winnerA.appendChild(winner.heading1);
+        }
+        else{
+            const winnerB=document.querySelector('.heading-player-two-name');
+            winnerB.appendChild(winner.heading1);
+        }
+        // const mainGridDiv=document.querySelector('.main-grid-div');
+        // mainGridDiv.addEventListener('click',()=>{
+        //     mainGridDiv.addEventListener('click',()=>{
+        //         resetGrid();
+        //     });
+        // });
+    }
+}
+
+const checkGame = () => {
+    check(0,1,2);
+    check(0,4,8);
+    check(0,3,6);
+    check(2,4,6);
+    check(2,5,8);
+    check(6,7,8);
+    check(3,4,5);
+    check(1,4,7);      
+}
+function resetGrid ()  {
+    for(let i=0;i<10;i++){
+        arr[i]=0;
+    }
+    const winner=document.querySelectorAll('.winner-class');
+    for(win of winner){
+        win.remove();
+    }
+    console.log('done');
+    render();
+}
